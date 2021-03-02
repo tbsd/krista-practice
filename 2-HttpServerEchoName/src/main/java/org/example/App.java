d106e5275b4cebb9684b35199e96432c1830fc93
Anton Kurbatov <a.kurbatov@protonmail.com>
2021-03-02 21:30:19 +0300

    replcae template with name echo server

--- 2-HttpServerEchoName/src/main/java/org/example/App.java
+++ 2-HttpServerEchoName/src/main/java/org/example/App.java
@@ -1,13 +1,25 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import io.undertow.Undertow;
+import io.undertow.util.Headers;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8000, "127.0.0.1")
+                .setHandler(exchange -> {
+                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
+                    exchange.getResponseSender()
+                            .send("<html>" +
+                                    "<head>" +
+                                    "<meta charset=\"UTF-8\">" +
+                                    "</head>" +
+                                    "<body>" +
+                                    "Курбатов Антон" +
+                                    "</body>" +
+                                    "</html>");
+                }).build();
+        server.start();
     }
 }
