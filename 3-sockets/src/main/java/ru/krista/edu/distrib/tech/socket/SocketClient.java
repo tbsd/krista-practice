6e432df1f8abf659968769a6240a663a74f64d43
Anton Kurbatov <a.kurbatov@protonmail.com>
2021-03-08 22:57:16 +0300

    add connection refused handling

--- 3-sockets/src/main/java/ru/krista/edu/distrib/tech/socket/SocketClient.java
+++ 3-sockets/src/main/java/ru/krista/edu/distrib/tech/socket/SocketClient.java
@@ -5,8 +5,12 @@ import java.io.IOException;
 public class SocketClient {
     public static void main(String[] args) throws IOException {
         EchoClient cli = new EchoClient();
-        cli.startConnection("127.0.0.1", 6000);
-        System.out.println(cli.sendMessage("test"));
-        cli.stopConnection();
+        try {
+            cli.startConnection("127.0.0.1", 6000);
+            System.out.println(cli.sendMessage("test"));
+            cli.stopConnection();
+        } catch (java.net.ConnectException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
