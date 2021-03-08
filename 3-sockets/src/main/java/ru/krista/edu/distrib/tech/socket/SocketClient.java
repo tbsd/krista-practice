762181405a18e562b8108dd21cc879da01736e1e
Anton Kurbatov <a.kurbatov@protonmail.com>
2021-03-08 22:51:46 +0300

    add SocksClient

---
+++ 3-sockets/src/main/java/ru/krista/edu/distrib/tech/socket/SocketClient.java
@@ -0,0 +1,12 @@
+package ru.krista.edu.distrib.tech.socket;
+
+import java.io.IOException;
+
+public class SocketClient {
+    public static void main(String[] args) throws IOException {
+        EchoClient cli = new EchoClient();
+        cli.startConnection("127.0.0.1", 6000);
+        System.out.println(cli.sendMessage("test"));
+        cli.stopConnection();
+    }
+}
