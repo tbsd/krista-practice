688b44047b4690a5b39f8caf89430ab77ac11119
Anton Kurbatov <a.kurbatov@protonmail.com>
2021-03-08 23:33:23 +0300

    add PortScanner

---
+++ 3-sockets/src/main/java/ru/krista/edu/distrib/tech/socket/PortScanner.java
@@ -0,0 +1,24 @@
+package ru.krista.edu.distrib.tech.socket;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class PortScanner {
+    public static void main(String[] args) throws IOException {
+        for (int i = 1; i <= 65535; ++i)
+            if (isPortInUse(i))
+                System.out.println(i + " is in use");
+    }
+
+    static boolean isPortInUse(int port) throws IOException {
+        try {
+            Socket socket = new Socket("127.0.0.1", port);
+            socket.close();
+            return true;
+        } catch (ConnectException ex) {
+            return false;
+        }
+    }
+}
