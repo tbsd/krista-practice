85757076d60d1499e120b5880e3d5ebddb9059f7
Anton Kurbatov <a.kurbatov@protonmail.com>
2021-03-31 12:09:48 +0200

    add test

--- 5-Rest/server/rest-json-quickstart/src/test/java/org/acme/rest/json/PersonResourceTest.java
+++ 5-Rest/server/rest-json-quickstart/src/test/java/org/acme/rest/json/PersonResourceTest.java
@@ -1,21 +1,47 @@
 package org.acme.rest.json;
 
 import io.quarkus.test.junit.QuarkusTest;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 public class PersonResourceTest {
+    OkHttpClient client = new OkHttpClient();
 
     @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/fruits")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+    public void testGetList() throws IOException {
+        Request request = new Request.Builder()
+                .url("http://localhost:8080/persons")
+                .build();
+        Call call = client.newCall(request);
+        Response response = call.execute();
+        String responseText = response.body().string();
+        String data = "[{\"firstName\":\"Ivan\",\"lastName\":\"Ivanovich\",\"address\":{\"streetAddress\":\"Petrovskaya St.\",\"city\":\"Moscow\",\"postalCode\":12345678},\"phoneNumbers\":[{\"phone\":\"8-123-321-2211\"}]},{\"firstName\":\"Vasyan\",\"lastName\":\"Vashyanovich\",\"address\":{\"streetAddress\":\"Razumovskaya St.\",\"city\":\"Yaroslavl\",\"postalCode\":12345632},\"phoneNumbers\":[{\"phone\":\"8-151-000-2185\"}]}]";
+        assertEquals(data, responseText);
     }
 
+    @Test
+    public void testAddRecord() {
+        // https://stackoverflow.com/questions/34886172/okhttp-put-example
+//        String newPerson =
+//        Request request = new Request.Builder()
+//                .url("http://localhost:8080/persons")
+//                .put(body)
+//                .build();
+
+    }
+
+    @Test
+    public void testGetOneRecord() {
+
+    }
 }
