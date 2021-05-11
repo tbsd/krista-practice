f03271788704cd98609ea815bd4fb489d488abd5
Anton Kurbatov <a.kurbatov@protonmail.com>
2021-05-12 00:03:47 +0300

    remove unused test

--- 8-Docker/3.2/src/test/java/org/acme/rest/json/PersonResourceTest.java
+++ 8-Docker/3.2/src/test/java/org/acme/rest/json/PersonResourceTest.java
@@ -15,18 +15,4 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 public class PersonResourceTest {
-    OkHttpClient client = new OkHttpClient();
-
-    @Test
-    public void testGetList() throws IOException {
-        Request request = new Request.Builder()
-                .url("http://localhost:8080/persons")
-                .build();
-        Call call = client.newCall(request);
-        Response response = call.execute();
-        String responseText = response.body().string();
-        String data = "[{\"firstName\":\"Ivan\",\"lastName\":\"Ivanovich\",\"address\":{\"streetAddress\":\"Petrovskaya St.\",\"city\":\"Moscow\",\"postalCode\":12345678},\"phoneNumbers\":[{\"phone\":\"8-123-321-2211\"}]},{\"firstName\":\"Vasyan\",\"lastName\":\"Vashyanovich\",\"address\":{\"streetAddress\":\"Razumovskaya St.\",\"city\":\"Yaroslavl\",\"postalCode\":12345632},\"phoneNumbers\":[{\"phone\":\"8-151-000-2185\"}]}]";
-        assertEquals(data, responseText);
-    }
-
 }
